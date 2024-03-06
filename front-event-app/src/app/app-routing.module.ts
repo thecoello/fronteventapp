0c86ef36cb6794c0e9aaa5a8406dfe43f74b17ff
thecoello <thecoello@gmail.com>
2024-03-06 09:21:00 +0100

    Configuración de ruta para componente de registro de usuario

--- front-event-app/src/app/app-routing.module.ts
+++ front-event-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistroUsuarioComponent } from './components/usuarios/registro-usuario/registro-usuario.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: HomeComponent},
+  {path: 'registro',component: RegistroUsuarioComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
