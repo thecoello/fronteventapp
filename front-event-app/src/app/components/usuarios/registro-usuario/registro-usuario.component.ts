956be225ca1094e1f2f3589234c8c9411be0b2d4
thecoello <thecoello@gmail.com>
2024-03-06 18:42:56 +0100

    Implementación de POST con api y validación de campos

--- front-event-app/src/app/components/usuarios/registro-usuario/registro-usuario.component.ts
+++ front-event-app/src/app/components/usuarios/registro-usuario/registro-usuario.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserAdmin } from 'src/app/models/users';
+import { UserAdminService } from 'src/app/services/user-admin.service';
+import {AbstractControl, ValidatorFn} from '@angular/forms';
 
 @Component({
   selector: 'app-registro-usuario',
@@ -9,15 +12,47 @@ import { UserAdmin } from 'src/app/models/users';
 })
 export class RegistroUsuarioComponent {
   
-
   modelUserAdmin: UserAdmin = new UserAdmin()
+  creatingUser: boolean = false
+  registerStatusOk?: string
+  registerStatusKo?: string
+  inputsValidate?: boolean = false
+
+  constructor(private userAdminService: UserAdminService, private router: Router) { }
 
 
-  onSubmit(registroUsuario: NgForm): void {
+  onSubmit(registroUsuario: NgForm, event: any): void {
 
+      this.creatingUser = true
+      this.modelUserAdmin = registroUsuario.value
+
+      if(this.modelUserAdmin.nombre && this.modelUserAdmin.apellido && this.modelUserAdmin.email && this.modelUserAdmin.usuario && this.modelUserAdmin.password && this.modelUserAdmin.userTerms){
+        this.userAdminService.postUser(this.modelUserAdmin).subscribe((response) =>{
+
+          setTimeout(() => {
+              this.creatingUser = false
+              this.registerStatusKo = ""
+              this.registerStatusOk = "Registro completado"
+              this.modelUserAdmin = new UserAdmin()
+          }, 1000)
   
+        },
+        (error)=>{
+          console.log(error)
+  
+          setTimeout(() => {
+            this.creatingUser = false
+            this.registerStatusOk = ""
+            this.registerStatusKo = error.error
+          }, 1000);
+       
+        }
+        )
+      }else{
+        this.creatingUser = false
+        this.inputsValidate = true
+      }
 
+     
   }
-
-
 }
