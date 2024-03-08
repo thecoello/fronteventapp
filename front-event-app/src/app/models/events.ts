a6ab0829e690debe663c371aa12f57b495e606cf
thecoello <thecoello@gmail.com>
2024-03-08 14:48:35 +0100

    Modelo de ventos y zonas (zona se incluirá solamente en eventos)

---
+++ front-event-app/src/app/models/events.ts
@@ -0,0 +1,17 @@
+import { Zones } from "./zones"
+
+export class Event {
+    image?: String
+    nombreEvento?: String
+    tipoEvento?: String
+    descripcion?: String
+    fechaInicio?: String
+    horeIncio?:String
+    fechaFinal?: String
+    horeFinal?: String
+    enlaceStreaming?: String
+    capacidadEvento?: Number
+    direccion?: String
+    zonas?: Array<Zones>
+}
+
---
+++ front-event-app/src/app/models/zones.ts
@@ -0,0 +1,5 @@
+export class Zones {
+    imageMapaZona?: String
+    nombreZona?: String
+    cantidadDeTickets?: String
+}
