4ee93d59cd1e99dd305aa3737a36e82120fbf580
Sonia Mori <sonymm6s@gmail.com>
2024-03-09 19:36:23 +0100

    Componente edición de usuario provisional

--- front-event-app/src/app/components/usuarios/edicion-usuario/edicion-usuario.component.ts
+++ front-event-app/src/app/components/usuarios/edicion-usuario/edicion-usuario.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { UserAdmin } from 'src/app/models/users';
+import { FormControl, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserAdminService } from 'src/app/services/user-admin.service';
+import {AbstractControl, ValidatorFn} from '@angular/forms';
 
 @Component({
   selector: 'app-edicion-usuario',
@@ -7,4 +12,47 @@ import { Component } from '@angular/core';
 })
 export class EdicionUsuarioComponent {
 
+  modelUserAdmin: UserAdmin = new UserAdmin()
+  creatingUser: boolean = false
+  registerStatusOk?: string
+  registerStatusKo?: string
+  inputsValidate?: boolean = false
+
+  constructor(private userAdminService: UserAdminService, private router: Router) { }
+
+
+  onSubmit(registroUsuario: NgForm, event: any): void {
+
+      this.creatingUser = true
+      this.modelUserAdmin = registroUsuario.value
+
+      if(this.modelUserAdmin.nombre && this.modelUserAdmin.apellido && this.modelUserAdmin.email && this.modelUserAdmin.usuario && this.modelUserAdmin.password && this.modelUserAdmin.userTerms){
+        this.userAdminService.postUser(this.modelUserAdmin).subscribe((response) =>{
+
+          setTimeout(() => {
+              this.creatingUser = false
+              this.registerStatusKo = ""
+              this.registerStatusOk = "Registro completado"
+              this.modelUserAdmin = new UserAdmin()
+          }, 1000)
+  
+        },
+        (error)=>{
+          console.log(error)
+  
+          setTimeout(() => {
+            this.creatingUser = false
+            this.registerStatusOk = ""
+            this.registerStatusKo = error.error
+          }, 1000);
+       
+        }
+        )
+      }else{
+        this.creatingUser = false
+        this.inputsValidate = true
+      }
+
+     
+  }
 }
