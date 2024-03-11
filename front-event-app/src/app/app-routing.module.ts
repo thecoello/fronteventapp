6cb1754801ecc9096c375fac384d10671ce3e7c6
thecoello <thecoello@gmail.com>
2024-03-11 13:18:59 +0100

    Modificación de ruta edición de usuario, cambio de estilos y copy

--- front-event-app/src/app/app-routing.module.ts
+++ front-event-app/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { EventoDetalleComponent } from './components/eventos/evento-detalle/even
 import { DetalleUsuarioComponent } from './components/usuarios/detalle-usuario/detalle-usuario.component';
 import { ResetearpasswordComponent } from './components/usuarios/resetear-password/resetearpassword.component';
 import { LoginComponent } from './components/login/login.component';
+import { EdicionUsuarioComponent } from './components/usuarios/edicion-usuario/edicion-usuario.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,10 +17,8 @@ const routes: Routes = [
   { path: 'resetearcontrasena', component: ResetearpasswordComponent },
   { path: 'crearevento', component: CreacioneventoComponent },
   { path: 'detalleevento/:id', component: EventoDetalleComponent },
-  { path: 'detalleusuario', component: DetalleUsuarioComponent },
+  { path: 'editarusuario', component: EdicionUsuarioComponent },
   { path: 'login', component: LoginComponent },
-
-
 ];
 
 @NgModule({
