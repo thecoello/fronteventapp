cbfc3864fed09b7f6eb272d06d53e7bc0b6da9ae
Daniel <danielgomezgonzalez4c@gmail.com>
2024-03-11 11:00:05 +0100

    previa Login Usuario

--- front-event-app/src/app/components/login/login.component.ts
+++ front-event-app/src/app/components/login/login.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { UserAdmin } from 'src/app/models/users';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  modelUserAdmin: UserAdmin = new UserAdmin();
 
-}
+  constructor() { }
+
+  loginadmin(LoginComponent: NgForm){}
+  }
+
