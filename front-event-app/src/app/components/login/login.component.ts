11928454f531fbc4ee759aec5739473e96b1c1a5
Daniel <danielgomezgonzalez4c@gmail.com>
2024-03-14 11:48:06 +0100

    validacion login

--- front-event-app/src/app/components/login/login.component.ts
+++ front-event-app/src/app/components/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { UserAdmin } from 'src/app/models/users';
 import { NgForm } from '@angular/forms';
+import { UserAdminService } from 'src/app/services/user-admin.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  modelUserAdmin: UserAdmin = new UserAdmin();
+  modelUserAdmin: UserAdmin = new UserAdmin()
+  inputsValidate?: boolean = false
 
-  constructor() { }
+  constructor(private userAdminService: UserAdminService, private router: Router) { }
+
 
   loginadmin(LoginComponent: NgForm){}
   }
